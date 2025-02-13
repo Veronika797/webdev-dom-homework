0189c252696487c1c9061f349501f6b5f9937592
MironProFit <mpf_3d@vk.com>
2025-02-13 17:06:30 +0500

    добавление динамическоие элементы лайка

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputName = document.getElementById('input-name');
 const inputComment = document.getElementById('input-comment');
 const buttonInput = document.getElementById('button');
 const errorClass = document.querySelector('.error');
+const likeButton = document.querySelector('.like-button');
 
 const currentDate = new Date();
 const formattedDate = formatDate(currentDate)
@@ -16,6 +17,66 @@ function formatDate (date) {
     
     return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
+
+const userComments = [
+    {name: 'Глеб Фокин', date: '12.02.22 12:18', text: 'Это будет первый комментарий на этой странице', likes: 3, like: true},
+    {name: 'Варвара Н.', date: '13.02.22 19:22', text: 'Мне нравится как оформлена эта страница! ❤', likes: 75, like: true}
+  ];
+
+
+
+const renderComments = () => {
+
+    const container = document.getElementById('list');
+
+    container.innerHTML = userComments.map((comment, index) => 
+
+        `<li class="comment">
+          <div class="comment-header">
+            <div class="title">${comment.name}</div>
+            <div>${comment.date}</div>
+          </div>
+          <div class="comment-body">
+            <div class="comment-text">${comment.text}</div>
+          </div>
+          <div class="comment-footer">
+            <div class="likes">
+              <span class="likes-counter">${comment.likes}</span>
+              <button class="like-button ${comment.like ? '-active-like' : ''}" data-index ="${index}"></button>
+            </div>
+          </div>
+        </li>`
+
+    ).join("");
+
+    const likeButtons = document.querySelectorAll('.like-button');
+
+    likeButtons.forEach(button => {
+        button.addEventListener('click', () => {
+
+            const index = button.dataset.index;
+            const comment = userComments[index];
+    
+            if (comment.like) {
+    
+                comment.likes--;
+    
+            } else {
+    
+                comment.likes++
+    
+            } 
+            comment.like = !comment.like;
+    
+            renderComments();
+        })
+        
+    })
+    
+};
+
+renderComments()
+
 
 buttonInput.addEventListener('click', () => {
 
