3d9a88996d1f80a96c2328ba779d8c4ab643ac21
Veronika797 <milchenkovd@gmail.com>
2025-03-31 14:54:07 +0300

    выполнена работа по АПИ1

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+import { fetchComments } from './api.js'
 import { displayComments } from './modules/displayComments.js'
 import { addReplyHandler } from './modules/eventHandler.js'
+import { updateComments } from './modules/comments.js'
 
-addReplyHandler()
-displayComments()
+fetchComments().then((data) => {
+    updateComments(data)
+    displayComments()
+    addReplyHandler()
+})
--- modules/comments.js
+++ modules/comments.js
@@ -1,20 +1,5 @@
-export const comments = [
-    {
-        name: 'Глеб Фокин',
-        date: '12.02.22 12:18',
-        text: 'Это будет первый комментарий на этой странице',
-        likes: 3,
-        liked: true,
-    },
-    {
-        name: 'Варвара Н.',
-        date: '13.02.22 19:22',
-        text: 'Мне нравится как оформлена эта страница! ❤',
-        likes: 75,
-        liked: false,
-    },
-]
+export let comments = []
 
-export function updateComments(newComment) {
-    comments.push(newComment)
+export const updateComments = (newComment) => {
+    comments = newComment
 }
